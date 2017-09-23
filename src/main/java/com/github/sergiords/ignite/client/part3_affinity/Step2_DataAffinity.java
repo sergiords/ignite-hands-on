d0a0830dd2ac62dc59d873390a6fae257191c781
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-09-23 18:57:47 +0200

    rework affinity exercises

--- src/main/java/com/github/sergiords/ignite/client/part3_affinity/Step2_DataAffinity.java
+++ src/main/java/com/github/sergiords/ignite/client/part3_affinity/Step2_DataAffinity.java
@@ -1,18 +1,27 @@
 package com.github.sergiords.ignite.client.part3_affinity;
 
-import com.github.sergiords.ignite.client.ClientStep;
 import com.github.sergiords.ignite.data.CacheData;
 import com.github.sergiords.ignite.data.Team;
 import com.github.sergiords.ignite.data.User;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.affinity.AffinityKey;
+import org.apache.ignite.configuration.CacheConfiguration;
 
+import javax.cache.Cache;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
-@SuppressWarnings({"unused", "ConstantConditions", "FieldCanBeLocal"})
-public class Step2_DataAffinity implements ClientStep {
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toMap;
+
+public class Step2_DataAffinity implements Runnable {
 
     private static final String CACHE_NAME = "my-data-affinity-cache";
 
@@ -28,7 +37,10 @@ public class Step2_DataAffinity implements ClientStep {
          * TODO:
          * - create a partitioned cache named "my-data-affinity-cache" just like in Step1_Cache
          */
-        this.cache = null;
+        CacheConfiguration<AffinityKey<Team>, List<User>> configuration = new CacheConfiguration<>(CACHE_NAME);
+        configuration.setCacheMode(CacheMode.PARTITIONED);
+
+        this.cache = ignite.getOrCreateCache(configuration);
     }
 
     @Override
@@ -54,7 +66,7 @@ public class Step2_DataAffinity implements ClientStep {
          * - return a new affinity key using team's country as the affinity key discriminator
          * - use new AffinityKey(...)
          */
-        return null;
+        return new AffinityKey<>(team, team.getCountry());
     }
 
     private void populateCache() {
@@ -66,6 +78,7 @@ public class Step2_DataAffinity implements ClientStep {
          * - use CacheData#teams to find teams
          * - use CacheData#users to find users for a team
          */
+        CacheData.teams().forEach(team -> cache.put(affinityKey(team), CacheData.users(team)));
     }
 
     private Map<String, List<Team>> findTeamsByNode() {
@@ -75,12 +88,24 @@ public class Step2_DataAffinity implements ClientStep {
          * - return a map containing (NodeId => locally stored Team List)
          * - use IgniteCache#localEntries to find local cache entries and do this in every node
          * - use System.getProperty("node.id") to retrieve NodeId
-         * - use Pair or HashMap.SimpleEntry if you need a pair structure
+         * - use HashMap.SimpleEntry if you need a pair structure
          * TIP:
          * - unfortunately localCacheEntries returns an Iterable... that's annoying
          * - use StreamSupport.stream(myIterator.spliterator(), false) to get a plain-old stream
          */
-        return null;
+        return ignite.compute()
+            .broadcast(() -> {
+
+                String nodeId = System.getProperty("node.id");
+
+                List<Team> teams = StreamSupport.stream(cache.localEntries().spliterator(), false)
+                    .map(Cache.Entry::getKey)
+                    .map(AffinityKey::key)
+                    .collect(toList());
+
+                return new HashMap.SimpleEntry<>(nodeId, teams);
+            })
+            .stream().collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     private List<User> topCommitterFromCountry(String country) {
@@ -93,13 +118,13 @@ public class Step2_DataAffinity implements ClientStep {
          * - unfortunately localCacheEntries returns an Iterable... that's annoying
          * - use StreamSupport.stream(myIterator.spliterator(), false) to get a plain-old stream
          */
-
-        return null;
-    }
-
-    @Override
-    public void close() {
-        ignite.destroyCache(CACHE_NAME);
+        return ignite.compute().affinityCall(CACHE_NAME, country, () ->
+            StreamSupport.stream(cache.localEntries().spliterator(), false)
+                .filter(entry -> entry.getKey().key().getCountry().equals(country))
+                .map(entry -> entry.getValue().stream().max(comparing(User::getCommits)))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList()));
     }
 
 }
