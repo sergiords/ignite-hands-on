44143a09918d4b46a8759921c2383078bc0aa9d0
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-10-03 23:05:46 +0200

    update DataAffinity exercise and initialize corresponding test

--- src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step3_DataAffinity.java
+++ src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step3_DataAffinity.java
@@ -1,6 +1,6 @@
 package com.github.sergiords.ignite.client.part2_data_grid;
 
-import com.github.sergiords.ignite.data.CacheData;
+import com.github.sergiords.ignite.data.Data;
 import com.github.sergiords.ignite.data.Team;
 import com.github.sergiords.ignite.data.User;
 import org.apache.ignite.Ignite;
@@ -10,18 +10,13 @@ import org.apache.ignite.cache.affinity.AffinityKey;
 import org.apache.ignite.configuration.CacheConfiguration;
 
 import javax.cache.Cache;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
 
-public class Step3_DataAffinity implements Runnable {
+public class Step3_DataAffinity {
 
     private static final String CACHE_NAME = "my-data-affinity-cache";
 
@@ -36,6 +31,7 @@ public class Step3_DataAffinity implements Runnable {
         /*
          * TODO:
          * - create a partitioned cache named "my-data-affinity-cache" just like in Step1_Cache
+         * - notice type for keys is AffinityKey<Team> not just Team
          */
         CacheConfiguration<AffinityKey<Team>, List<User>> configuration = new CacheConfiguration<>(CACHE_NAME);
         configuration.setCacheMode(CacheMode.PARTITIONED);
@@ -43,23 +39,7 @@ public class Step3_DataAffinity implements Runnable {
         this.cache = ignite.getOrCreateCache(configuration);
     }
 
-    @Override
-    public void run() {
-
-        populateCache();
-
-        Map<String, List<Team>> teamsByNode = findTeamsByNode();
-
-        String country = CacheData.getCountries().get(0);
-
-        List<User> topCommitterFromCountry = topCommitterFromCountry(country);
-
-        System.out.printf("===== DataAffinity =====%n");
-        System.out.printf("=> Teams by Node: %s%n", teamsByNode);
-        System.out.printf("=> Top committers from %s are %s%n", country, topCommitterFromCountry);
-    }
-
-    private AffinityKey<Team> affinityKey(Team team) {
+    public AffinityKey<Team> affinityKey(Team team) {
 
         /*
          * TODO:
@@ -69,62 +49,37 @@ public class Step3_DataAffinity implements Runnable {
         return new AffinityKey<>(team, team.getCountry());
     }
 
-    private void populateCache() {
+    public void populateCache() {
 
         /*
          * TODO:
          * - populate cache
          * - put all teams from the same country in the same node using affinityKey method defined above
-         * - use CacheData#teams to find teams
-         * - use CacheData#users to find users for a team
+         * - use Data.teams() to find teams
+         * - use Data.users(team) to find users for a team
          */
-        CacheData.teams().forEach(team -> cache.put(affinityKey(team), CacheData.users(team)));
+        Data.teams().forEach(team -> cache.put(affinityKey(team), Data.users(team)));
     }
 
-    private Map<String, List<Team>> findTeamsByNode() {
+    public Optional<User> findTopCommitterFromCountry(String country) {
 
         /*
          * TODO:
-         * - return a map containing (NodeId => locally stored Team List)
-         * - use IgniteCache#localEntries to find local cache entries and do this in every node
-         * - use System.getProperty("node.id") to retrieve NodeId
-         * - use HashMap.SimpleEntry if you need a pair structure
+         * - return top committer for the given country
+         *
+         * - keep in mind that all teams from a country are hosted in same node
+         * - you can safely use ignite.compute().affinityCall(...) to make an affinity search in proper node (use country as affinity key)
+         * - on this node, use cache.localEntries() to get all locally stored teams
          * TIP:
-         * - unfortunately localCacheEntries returns an Iterable... that's annoying
-         * - use StreamSupport.stream(myIterator.spliterator(), false) to get a plain-old stream
-         */
-        return ignite.compute()
-            .broadcast(() -> {
-
-                String nodeId = System.getProperty("node.id");
-
-                List<Team> teams = StreamSupport.stream(cache.localEntries().spliterator(), false)
-                    .map(Cache.Entry::getKey)
-                    .map(AffinityKey::key)
-                    .collect(toList());
-
-                return new HashMap.SimpleEntry<>(nodeId, teams);
-            })
-            .stream().collect(toMap(Map.Entry::getKey, Map.Entry::getValue));
-    }
-
-    private List<User> topCommitterFromCountry(String country) {
-
-        /*
-         * TODO:
-         * - return top committer from each team from the given country
-         * - since each team from the same country are hosted in same node you can safely use IgniteCompute#affinityCall
-         * TIP:
-         * - unfortunately localCacheEntries returns an Iterable... that's annoying
+         * - cache.localEntries() returns an Iterable... that's annoying
          * - use StreamSupport.stream(myIterator.spliterator(), false) to get a plain-old stream
          */
         return ignite.compute().affinityCall(CACHE_NAME, country, () ->
             StreamSupport.stream(cache.localEntries().spliterator(), false)
                 .filter(entry -> entry.getKey().key().getCountry().equals(country))
-                .map(entry -> entry.getValue().stream().max(comparing(User::getCommits)))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .collect(Collectors.toList()));
+                .map(Cache.Entry::getValue)
+                .flatMap(List::stream)
+                .max(comparing(User::getCommits)));
     }
 
 }
---
+++ src/test/java/com/github/sergiords/ignite/client/part2_data_grid/Step2_DataAffinityTest.java
@@ -0,0 +1,67 @@
+package com.github.sergiords.ignite.client.part2_data_grid;
+
+import com.github.sergiords.ignite.data.Data;
+import com.github.sergiords.ignite.data.Team;
+import com.github.sergiords.ignite.data.User;
+import com.github.sergiords.ignite.server.ServerAppTest;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.affinity.AffinityKey;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+@ExtendWith(ServerAppTest.class)
+class Step2_DataAffinityTest {
+
+    private final Ignite ignite;
+
+    private final Step3_DataAffinity step;
+
+    public Step2_DataAffinityTest(Ignite ignite) {
+        this.ignite = ignite;
+        this.step = new Step3_DataAffinity(ignite);
+    }
+
+    @TestFactory
+    @DisplayName("Data affinity")
+    List<DynamicTest> computeAffinity() {
+
+        String name = "my-data-affinity-cache";
+        IgniteCache<Team, List<User>> cache = ignite.cache(name);
+
+        Team team42 = Data.teams().get(42);
+
+        return asList(
+
+            dynamicTest("populateCache() should store 1000 teams in cache", () -> {
+                step.populateCache();
+                assertThat(cache.size()).isEqualTo(1000);
+            }),
+
+            dynamicTest("affinityKey() should return key for team and its country", () -> {
+                AffinityKey<Team> result = step.affinityKey(team42);
+                assertThat(result).isNotNull();
+                assertThat(result.key()).isEqualTo(team42);
+                assertThat(result.<String>affinityKey()).isEqualTo(team42.getCountry());
+            }),
+
+            dynamicTest("findTopCommitterFromCountry() should return top committer from country", () -> {
+                Optional<User> topCommitterFromCountry = step.findTopCommitterFromCountry(Locale.FRANCE.getDisplayCountry());
+                assertThat(topCommitterFromCountry)
+                    .isNotEmpty().map(User::getId).contains(423);
+            })
+
+        );
+    }
+
+}
