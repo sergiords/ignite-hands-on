caac9de5c9cf29aa36c02c2d1cdb960ea8909be1
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-10-04 23:48:52 +0200

    simplify ComputeAffinityTest

--- src/test/java/com/github/sergiords/ignite/client/part2_data_grid/Step2_ComputeAffinityTest.java
+++ src/test/java/com/github/sergiords/ignite/client/part2_data_grid/Step2_ComputeAffinityTest.java
@@ -3,7 +3,6 @@ package com.github.sergiords.ignite.client.part2_data_grid;
 import com.github.sergiords.ignite.data.Data;
 import com.github.sergiords.ignite.data.Team;
 import com.github.sergiords.ignite.data.User;
-import com.github.sergiords.ignite.server.ServerApp;
 import com.github.sergiords.ignite.server.ServerAppTest;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
@@ -14,15 +13,9 @@ import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
 
 import static java.util.Arrays.asList;
-import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toMap;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
@@ -50,50 +43,18 @@ class Step2_ComputeAffinityTest {
         ClusterNode team42Node = affinity.mapKeyToNode(team42);
         String team42NodeName = team42Node.attribute("node.id");
 
-        List<String> otherNodes = ignite.cluster()
-            .forPredicate(node -> !node.equals(team42Node))
-            .nodes().stream()
-            .map(node -> node.<String>attribute("node.id"))
-            .filter(Objects::nonNull)
-            .collect(toList());
-
         return asList(
 
             dynamicTest("cache should have 1000 teams", () -> assertThat(cache.size()).isEqualTo(1000)),
 
-            dynamicTest("findTopCommitter() should be executed on node " + team42NodeName, () -> {
-                ignite.cluster().resetMetrics();
-
+            dynamicTest("findTopCommitter() should be executed on node " + team42NodeName, () ->
                 assertThat(step2.findTopCommitter(team42))
-                    .hasValueSatisfying(user -> assertThat(user.getId()).isEqualTo(423));
+                    .hasValueSatisfying(user -> assertThat(user.getId()).isEqualTo(423))),
 
-                checkAffinityCall(team42NodeName, otherNodes);
-            }),
-
-            dynamicTest("findTopCommitterFullVersion() should be executed on node " + team42NodeName, () -> {
-                ignite.cluster().resetMetrics();
-
+            dynamicTest("findTopCommitterFullVersion() should be executed on node " + team42NodeName, () ->
                 assertThat(step2.findTopCommitterFullVersion(team42))
-                    .hasValueSatisfying(user -> assertThat(user.getId()).isEqualTo(423));
-
-                checkAffinityCall(team42NodeName, otherNodes);
-            })
+                    .hasValueSatisfying(user -> assertThat(user.getId()).isEqualTo(423)))
         );
-    }
-
-    private void checkAffinityCall(String team42NodeName, List<String> otherNodes) {
-
-        Map<String, Integer> jobsByNode = ignite.compute().broadcast(() -> new SimpleEntry<>(
-            ServerApp.getName(),
-            ignite.cluster().forLocal().metrics().getTotalExecutedJobs()
-        )).stream().collect(toMap(Entry::getKey, Entry::getValue));
-
-        // there should be more jobs executed on team42 node than other nodes
-        Integer team42NodeCount = jobsByNode.get(team42NodeName);
-        otherNodes.forEach(otherNodeName -> {
-            Integer otherNodeCount = jobsByNode.get(otherNodeName);
-            assertThat(team42NodeCount).isGreaterThan(otherNodeCount);
-        });
     }
 
 }
