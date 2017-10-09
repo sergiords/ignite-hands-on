90b20d277e81ee92713f042740b1022e4ee28bd4
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-10-09 22:52:31 +0200

    extract cache queries exercises into dedicated step and refactor cache tests

--- src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step1_Cache.java
+++ src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step1_Cache.java
@@ -1,22 +1,12 @@
 package com.github.sergiords.ignite.client.part2_data_grid;
 
-import com.github.sergiords.ignite.data.Data;
-import com.github.sergiords.ignite.data.Team;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.cache.CacheEntryProcessor;
 import org.apache.ignite.cache.CacheMode;
 import org.apache.ignite.cache.CachePeekMode;
-import org.apache.ignite.cache.query.ScanQuery;
-import org.apache.ignite.cache.query.SqlQuery;
-import org.apache.ignite.cache.query.TextQuery;
 import org.apache.ignite.configuration.CacheConfiguration;
 
-import javax.cache.Cache.Entry;
 import java.util.Collection;
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
 
 public class Step1_Cache {
 
@@ -26,7 +16,7 @@ public class Step1_Cache {
         this.ignite = ignite;
     }
 
-    public IgniteCache<Integer, Team> createPartitionedCache(Ignite ignite) {
+    public IgniteCache<Integer, String> createPartitionedCache(Ignite ignite) {
 
         /*
          * TODO:
@@ -34,100 +24,14 @@ public class Step1_Cache {
          * - configure cache mode to PARTITIONED
          * - create and return a cache with this configuration
          */
-        CacheConfiguration<Integer, Team> configuration = new CacheConfiguration<>();
+        CacheConfiguration<Integer, String> configuration = new CacheConfiguration<>();
         configuration.setName("my-partitioned-cache");
         configuration.setCacheMode(CacheMode.PARTITIONED);
-        configuration.setIndexedTypes(Integer.class, Team.class);
 
         return ignite.getOrCreateCache(configuration);
     }
 
-    public void writeValues(IgniteCache<Integer, Team> cache) {
-
-        List<Team> teams = Data.teams();
-
-        /*
-         * TODO:
-         * - put all teams in the cache using team.getId() as cache key
-         */
-        teams.forEach(team -> cache.put(team.getId(), team));
-    }
-
-    public Team findByKey(IgniteCache<Integer, Team> cache, Integer id) {
-
-        /*
-         * TODO:
-         * - find team in cache by id
-         */
-        return cache.get(id);
-    }
-
-    public List<Team> findByScanQuery(IgniteCache<Integer, Team> cache, String nameSearch) {
-
-        // Scan query
-
-        /*
-         * TODO:
-         * - create a ScanQuery finding teams whose name ends with 'nameSearch'
-         * - use cache.query(...) to return teams from cache matching this query
-         */
-        ScanQuery<Integer, Team> scanQuery = new ScanQuery<>((key, value) -> value.getName().endsWith(nameSearch));
-
-        return cache.query(scanQuery).getAll().stream()
-            .map(Entry::getValue)
-            .collect(toList());
-    }
-
-    public List<Team> findByTextQuery(IgniteCache<Integer, Team> cache, String nameSearch) {
-
-        // Text Query
-
-        /*
-         * TODO:
-         * - add @QueryTextField annotation to Team.name field to allow Lucene-based Text search on name
-         * - create a TextQuery finding teams matching 'nameSearch'
-         * - use cache.query(...) to return teams from cache matching this query
-         */
-        TextQuery<Integer, Team> textQuery = new TextQuery<>(Team.class, nameSearch);
-
-        return cache.query(textQuery).getAll().stream()
-            .map(Entry::getValue)
-            .collect(toList());
-    }
-
-    public List<Team> findBySqlQuery(IgniteCache<Integer, Team> cache, String nameSearch) {
-
-        // SQL Query
-
-        /*
-         * TODO:
-         * - add @QuerySqlField annotation to Team.name field to allow SQL query name searches
-         * - create a SqlQuery finding teams where name is like 'nameSearch'
-         * - query example: 'select * from team where name like ?'
-         * - use cache.query(...) to return teams from cache matching this query
-         */
-        SqlQuery<Integer, Team> sqlQuery = new SqlQuery<Integer, Team>(Team.class, "name like ?").setArgs(nameSearch);
-
-        return cache.query(sqlQuery).getAll().stream()
-            .map(Entry::getValue)
-            .collect(toList());
-    }
-
-    public String processEntry(IgniteCache<Integer, Team> cache, Integer id) {
-
-        // Entry Processor
-
-        /*
-         * TODO:
-         * - create a CacheEntryProcessor returning team name in uppercase
-         * - use cache.invoke(...) to return the processed name of the team with given id
-         */
-        CacheEntryProcessor<Integer, Team, String> processor = (entry, args) -> entry.getValue().getName().toUpperCase();
-
-        return cache.invoke(id, processor);
-    }
-
-    public IgniteCache<Integer, Team> createReplicatedCache(Ignite ignite) {
+    public IgniteCache<Integer, String> createReplicatedCache(Ignite ignite) {
 
         /*
          * TODO:
@@ -135,15 +39,14 @@ public class Step1_Cache {
          * - configure cache mode to REPLICATED
          * - create and return a cache with this configuration
          */
-        CacheConfiguration<Integer, Team> configuration = new CacheConfiguration<>();
+        CacheConfiguration<Integer, String> configuration = new CacheConfiguration<>();
         configuration.setName("my-replicated-cache");
         configuration.setCacheMode(CacheMode.REPLICATED);
-        configuration.setIndexedTypes(Integer.class, Team.class);
 
         return ignite.getOrCreateCache(configuration);
     }
 
-    public IgniteCache<Integer, Team> createBackupCache(Ignite ignite) {
+    public IgniteCache<Integer, String> createBackupCache(Ignite ignite) {
 
         /*
          * TODO:
@@ -152,16 +55,15 @@ public class Step1_Cache {
          * - configure cache to have 1 backup exactly for each entry
          * - create and return a cache with this configuration
          */
-        CacheConfiguration<Integer, Team> configuration = new CacheConfiguration<>();
+        CacheConfiguration<Integer, String> configuration = new CacheConfiguration<>();
         configuration.setName("my-backup-cache");
         configuration.setBackups(1);
         configuration.setCacheMode(CacheMode.PARTITIONED);
-        configuration.setIndexedTypes(Integer.class, Team.class);
 
         return ignite.getOrCreateCache(configuration);
     }
 
-    public Integer getCacheSize(IgniteCache<Integer, Team> cache) {
+    public Integer getCacheSize(IgniteCache<Integer, String> cache) {
 
         /*
          * TODO:
@@ -171,7 +73,7 @@ public class Step1_Cache {
         return cache.size();
     }
 
-    public Collection<Integer> getCacheSizePerNode(IgniteCache<Integer, Team> cache) {
+    public Collection<Integer> getCacheSizePerNode(IgniteCache<Integer, String> cache) {
 
         /*
          * TODO:
@@ -182,7 +84,7 @@ public class Step1_Cache {
         return ignite.compute().broadcast(() -> cache.localSize(CachePeekMode.ALL));
     }
 
-    public Collection<Integer> getCacheSizePerNodeForPrimaryKeys(IgniteCache<Integer, Team> cache) {
+    public Collection<Integer> getCacheSizePerNodeForPrimaryKeys(IgniteCache<Integer, String> cache) {
 
         /*
          * TODO:
@@ -193,7 +95,7 @@ public class Step1_Cache {
         return ignite.compute().broadcast(() -> cache.localSize(CachePeekMode.PRIMARY));
     }
 
-    public Collection<Integer> getCacheSizePerNodeForBackupKeys(IgniteCache<Integer, Team> cache) {
+    public Collection<Integer> getCacheSizePerNodeForBackupKeys(IgniteCache<Integer, String> cache) {
 
         /*
          * TODO:
--- src/test/java/com/github/sergiords/ignite/client/part2_data_grid/Step1_CacheTest.java
+++ src/test/java/com/github/sergiords/ignite/client/part2_data_grid/Step1_CacheTest.java
@@ -1,7 +1,5 @@
 package com.github.sergiords.ignite.client.part2_data_grid;
 
-import com.github.sergiords.ignite.data.Data;
-import com.github.sergiords.ignite.data.Team;
 import com.github.sergiords.ignite.server.ServerAppTest;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
@@ -12,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,66 +29,71 @@ public class Step1_CacheTest {
     }
 
     @TestFactory
-    @DisplayName("should create a partitioned cache")
+    @DisplayName("partitioned cache")
     List<DynamicTest> partitionedCache() {
 
-        return testCacheUsage(step1.createPartitionedCache(ignite), "my-partitioned-cache", 1, 0);
+        IgniteCache<Integer, String> partitionedCache = step1.createPartitionedCache(ignite);
+
+        return testCacheUsage(partitionedCache, "my-partitioned-cache", 1, 0);
     }
 
     @TestFactory
-    @DisplayName("should create a replicated cache")
+    @DisplayName("replicated cache")
     List<DynamicTest> replicatedCache() {
 
-        return testCacheUsage(step1.createReplicatedCache(ignite), "my-replicated-cache", 3, 2);
+        IgniteCache<Integer, String> replicatedCache = step1.createReplicatedCache(ignite);
+
+        return testCacheUsage(replicatedCache, "my-replicated-cache", 3, 2);
     }
 
     @TestFactory
-    @DisplayName("should create a backup cache")
+    @DisplayName("backup cache")
     List<DynamicTest> backupCache() {
 
-        return testCacheUsage(step1.createBackupCache(ignite), "my-backup-cache", 2, 1);
+        IgniteCache<Integer, String> backupCache = step1.createBackupCache(ignite);
+
+        return testCacheUsage(backupCache, "my-backup-cache", 2, 1);
     }
 
-    private List<DynamicTest> testCacheUsage(IgniteCache<Integer, Team> cache, String name, int allFactor, int backupFactor) {
+    private List<DynamicTest> testCacheUsage(IgniteCache<Integer, String> cache, String name, int allFactor, int backupFactor) {
 
-        int expected = Data.teams().size();
+        int expected = 1000;
         int all = expected * allFactor;
         int backups = expected * backupFactor;
 
         return asList(
 
             // Name
-            dynamicTest("name", () -> {
+            dynamicTest("should be named " + name, () -> {
                 assertThat(cache).isNotNull();
                 assertThat(cache.getName()).isEqualTo(name);
             }),
 
             // Write
-            dynamicTest("write values", () -> {
-                step1.writeValues(cache);
+            dynamicTest("should have 1000 entries", () -> {
+                IntStream.range(0, expected).boxed().forEach(i -> cache.put(i, String.valueOf(i)));
                 assertThat(cache.size()).isEqualTo(expected);
             }),
 
-            // Queries
-            dynamicTest("get value", () -> assertThat(step1.findByKey(cache, 42))
-                .isNotNull().satisfies(team -> assertThat(team.getId()).isEqualTo(42))),
-            dynamicTest("scan query", () -> assertThat(step1.findByScanQuery(cache, "99"))
-                .extracting(Team::getId).hasSize(10).allMatch(id -> id % 100 == 99)),
-            dynamicTest("text query", () -> assertThat(step1.findByTextQuery(cache, "Team*99"))
-                .extracting(Team::getId).hasSize(10).allMatch(id -> id % 100 == 99)),
-            dynamicTest("sql query", () -> assertThat(step1.findBySqlQuery(cache, "%99"))
-                .extracting(Team::getId).hasSize(10).allMatch(id -> id % 100 == 99)),
-            dynamicTest("process entry", () -> assertThat(step1.processEntry(cache, 99))
-                .isNotNull().isEqualTo("TEAM99")),
+            dynamicTest("should have cluster size: " + expected, () -> {
+                Integer result = step1.getCacheSize(cache);
+                assertThat(result).isEqualTo(expected);
+            }),
 
-            // Size
-            dynamicTest("cache size", () -> assertThat(step1.getCacheSize(cache)).isEqualTo(expected)),
-            dynamicTest("cache size (ALL)", () -> assertThat(sum(step1.getCacheSizePerNode(cache)))
-                .isEqualTo(all)),
-            dynamicTest("cache size (PRIMARY)", () -> assertThat(sum(step1.getCacheSizePerNodeForPrimaryKeys(cache)))
-                .isEqualTo(expected)),
-            dynamicTest("cache size (BACKUP)", () -> assertThat(sum(step1.getCacheSizePerNodeForBackupKeys(cache)))
-                .isEqualTo(backups))
+            dynamicTest("should have ALL entries: " + all, () -> {
+                int result = sum(step1.getCacheSizePerNode(cache));
+                assertThat(result).isEqualTo(all);
+            }),
+
+            dynamicTest("should have PRIMARY entries: " + expected, () -> {
+                int result = sum(step1.getCacheSizePerNodeForPrimaryKeys(cache));
+                assertThat(result).isEqualTo(expected);
+            }),
+
+            dynamicTest("should have BACKUP entries: " + backups, () -> {
+                int result = sum(step1.getCacheSizePerNodeForBackupKeys(cache));
+                assertThat(result).isEqualTo(backups);
+            })
         );
     }
 
