683c7f34c5e3d13ae174ef35d47044e6904f84d7
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-10-09 22:52:13 +0200

    extract cache queries exercises into dedicated step

---
+++ src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step2_CacheQuery.java
@@ -0,0 +1,125 @@
+package com.github.sergiords.ignite.client.part2_data_grid;
+
+import com.github.sergiords.ignite.data.Data;
+import com.github.sergiords.ignite.data.Team;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.CacheEntryProcessor;
+import org.apache.ignite.cache.query.ScanQuery;
+import org.apache.ignite.cache.query.SqlQuery;
+import org.apache.ignite.cache.query.TextQuery;
+import org.apache.ignite.configuration.CacheConfiguration;
+
+import javax.cache.Cache.Entry;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class Step2_CacheQuery {
+
+    private final IgniteCache<Integer, Team> cache;
+
+    public Step2_CacheQuery(Ignite ignite) {
+
+        /*
+         * TODO:
+         * - create a CacheConfiguration named "my-cache"
+         * - create a cache with this configuration
+         */
+        CacheConfiguration<Integer, Team> configuration = new CacheConfiguration<>();
+        configuration.setName("my-cache");
+        configuration.setIndexedTypes(Integer.class, Team.class);
+
+        this.cache = ignite.getOrCreateCache(configuration);
+    }
+
+    public void writeValues() {
+
+        List<Team> teams = Data.teams();
+
+        /*
+         * TODO:
+         * - put all teams in the cache using team.getId() as cache key
+         */
+        teams.forEach(team -> cache.put(team.getId(), team));
+    }
+
+    public Team findByKey(Integer id) {
+
+        /*
+         * TODO:
+         * - find team in cache by id
+         */
+        return cache.get(id);
+    }
+
+    public String findByKeyAndProcess(Integer id) {
+
+        // Entry Processor
+
+        /*
+         * TODO:
+         * - create a CacheEntryProcessor returning team name in uppercase
+         * - use cache.invoke(...) to return the processed name of the team with given id
+         */
+        CacheEntryProcessor<Integer, Team, String> processor = (entry, args) -> entry.getValue().getName().toUpperCase();
+
+        return cache.invoke(id, processor);
+    }
+
+    public List<Team> findByScanQuery(String nameSearch) {
+
+        // Scan query
+
+        /*
+         * TODO:
+         * - create a ScanQuery finding teams whose name ends with given nameSearch
+         * - use cache.query(...) to return teams from cache matching this query
+         */
+        ScanQuery<Integer, Team> scanQuery = new ScanQuery<>((key, value) -> value.getName().endsWith(nameSearch));
+
+        return cache.query(scanQuery).getAll().stream()
+            .map(Entry::getValue)
+            .collect(toList());
+    }
+
+    public List<Team> findByTextQuery(String nameSearch) {
+
+        // Text Query
+
+        /*
+         * TODO:
+         * - add @QueryTextField annotation to Team.name field to allow Lucene-based Text search on name
+         * - create a TextQuery finding teams matching 'nameSearch'
+         * - use cache.query(...) to return teams from cache matching this query
+         * TIP:
+         * - define indexed types (cache key and cache value types) in cache configuration
+         */
+        TextQuery<Integer, Team> textQuery = new TextQuery<>(Team.class, nameSearch);
+
+        return cache.query(textQuery).getAll().stream()
+            .map(Entry::getValue)
+            .collect(toList());
+    }
+
+    public List<Team> findBySqlQuery(String nameSearch) {
+
+        // SQL Query
+
+        /*
+         * TODO:
+         * - add @QuerySqlField annotation to Team.name field to allow SQL query name searches
+         * - create a SqlQuery finding teams where name is like 'nameSearch'
+         * - query example: 'select * from team where name like ?'
+         * - use cache.query(...) to return teams from cache matching this query
+         * TIP:
+         * - define indexed types (cache key and cache value types) in cache configuration
+         */
+        SqlQuery<Integer, Team> sqlQuery = new SqlQuery<Integer, Team>(Team.class, "name like ?").setArgs(nameSearch);
+
+        return cache.query(sqlQuery).getAll().stream()
+            .map(Entry::getValue)
+            .collect(toList());
+    }
+
+}
---
+++ src/test/java/com/github/sergiords/ignite/client/part2_data_grid/Step2_CacheQueryTest.java
@@ -0,0 +1,77 @@
+package com.github.sergiords.ignite.client.part2_data_grid;
+
+import com.github.sergiords.ignite.data.Team;
+import com.github.sergiords.ignite.server.ServerAppTest;
+import org.apache.ignite.Ignite;
+import org.apache.ignite.IgniteCache;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+
+@ExtendWith(ServerAppTest.class)
+public class Step2_CacheQueryTest {
+
+    private final Ignite ignite;
+
+    private final Step2_CacheQuery step;
+
+    public Step2_CacheQueryTest(Ignite ignite) {
+        this.ignite = ignite;
+        this.step = new Step2_CacheQuery(ignite);
+    }
+
+    @TestFactory
+    @DisplayName("cache query step")
+    List<DynamicTest> cacheQuery() {
+
+        IgniteCache<Integer, Team> cache = ignite.cache("my-cache");
+
+        return asList(
+
+            dynamicTest("should create a cache named \"my-cache\"", () -> {
+                assertThat(cache).isNotNull();
+                assertThat(cache.getName()).isEqualTo("my-cache");
+            }),
+
+            dynamicTest("should write 1000 values to cache", () -> {
+                step.writeValues();
+                assertThat(cache.size()).isEqualTo(1000);
+            }),
+
+            dynamicTest("should find team by id", () -> {
+                Team result = step.findByKey(42);
+                assertThat(result).isNotNull();
+                assertThat(result.getId()).isEqualTo(42);
+            }),
+
+            dynamicTest("should find and process team by id", () -> {
+                String result = step.findByKeyAndProcess(99);
+                assertThat(result).isNotNull().isEqualTo("TEAM99");
+            }),
+
+            dynamicTest("should find teams with scan query", () -> {
+                List<Team> result = step.findByScanQuery("99");
+                assertThat(result).hasSize(10).extracting(Team::getId).allMatch(id -> id % 100 == 99);
+            }),
+
+            dynamicTest("should find teams with text query", () -> {
+                List<Team> result = step.findByTextQuery("Team*99");
+                assertThat(result).hasSize(10).extracting(Team::getId).allMatch(id -> id % 100 == 99);
+            }),
+
+            dynamicTest("should find teams with sql query", () -> {
+                List<Team> result = step.findBySqlQuery("%99");
+                assertThat(result).hasSize(10).extracting(Team::getId).allMatch(id -> id % 100 == 99);
+            })
+
+        );
+    }
+
+}
