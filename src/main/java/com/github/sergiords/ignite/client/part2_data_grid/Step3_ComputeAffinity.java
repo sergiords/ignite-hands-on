9e18022164fc98da2196d5c210f88509136ce871
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-11-23 21:49:55 +0100

    remove solutions for Step3_ComputeAffinity

--- src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step3_ComputeAffinity.java
+++ src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step3_ComputeAffinity.java
@@ -1,19 +1,14 @@
 package com.github.sergiords.ignite.client.part2_data_grid;
 
-import com.github.sergiords.ignite.data.Data;
 import com.github.sergiords.ignite.data.Team;
 import com.github.sergiords.ignite.data.User;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.cache.affinity.Affinity;
 import org.apache.ignite.cluster.ClusterGroup;
 import org.apache.ignite.cluster.ClusterNode;
-import org.apache.ignite.configuration.CacheConfiguration;
 
 import java.util.List;
 import java.util.Optional;
-
-import static java.util.Comparator.comparing;
 
 public class Step3_ComputeAffinity {
 
@@ -31,9 +26,8 @@ public class Step3_ComputeAffinity {
          * TODO:
          * - create a cache named "my-compute-affinity-cache"
          */
-        CacheConfiguration<Team, List<User>> configuration = new CacheConfiguration<>(CACHE_NAME);
 
-        this.cache = ignite.getOrCreateCache(configuration);
+        this.cache = null;
     }
 
     public void populateCache() {
@@ -44,7 +38,6 @@ public class Step3_ComputeAffinity {
          * - use Data.teams() to find teams
          * - use Data.users(team) to find users for a team
          */
-        Data.teams().forEach(team -> cache.put(team, Data.users(team)));
     }
 
     public Optional<User> findTopCommitter(Team team) {
@@ -55,8 +48,7 @@ public class Step3_ComputeAffinity {
          * - notice that cache.get(team) call does not require network since team is hosted on node where computation is sent
          */
 
-        return ignite.compute()
-            .affinityCall(CACHE_NAME, team, () -> cache.get(team).stream().max(comparing(User::getCommits)));
+        return null;
     }
 
     public ClusterNode findNode(Team team) {
@@ -67,9 +59,8 @@ public class Step3_ComputeAffinity {
          * - use ignite.affinity(...) to get affinity function associated to this cache
          * - use affinity.mapKeyToNode(...) to get ClusterNode hosting the given team entry
          */
-        Affinity<Team> affinity = ignite.affinity(CACHE_NAME);
 
-        return affinity.mapKeyToNode(team);
+        return null;
     }
 
     public Optional<User> findTopCommitterFullVersion(Team team) {
@@ -80,7 +71,7 @@ public class Step3_ComputeAffinity {
          * TODO:
          * - use ignite.cluster().forNode(...) to get a ClusterGroup with previous node only
          */
-        ClusterGroup clusterGroup = ignite.cluster().forNode(node);
+        ClusterGroup clusterGroup = null;
 
         /*
          * TODO:
@@ -88,8 +79,7 @@ public class Step3_ComputeAffinity {
          * - notice this is the long version of ignite.compute().affinityCall(...)
          */
 
-        return ignite.compute(clusterGroup)
-            .call(() -> cache.get(team).stream().max(comparing(User::getCommits)));
+        return null;
     }
 
 }
