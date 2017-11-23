8cff4bb141cc6894155a01de4e238de0e780e7d7
Sergio Dos Santos <sergiords@users.noreply.github.com>
2017-11-23 21:41:54 +0100

    remove solutions for Step2_CacheMode

--- src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step2_CacheMode.java
+++ src/main/java/com/github/sergiords/ignite/client/part2_data_grid/Step2_CacheMode.java
@@ -2,9 +2,6 @@ package com.github.sergiords.ignite.client.part2_data_grid;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
-import org.apache.ignite.cache.CacheMode;
-import org.apache.ignite.cache.CachePeekMode;
-import org.apache.ignite.configuration.CacheConfiguration;
 
 import java.util.Collection;
 
@@ -24,11 +21,8 @@ public class Step2_CacheMode {
          * - configure cache mode to PARTITIONED
          * - create and return a cache with this configuration
          */
-        CacheConfiguration<Integer, String> configuration = new CacheConfiguration<>();
-        configuration.setName("my-partitioned-cache");
-        configuration.setCacheMode(CacheMode.PARTITIONED);
 
-        return ignite.getOrCreateCache(configuration);
+        return null;
     }
 
     public IgniteCache<Integer, String> createReplicatedCache(Ignite ignite) {
@@ -39,11 +33,8 @@ public class Step2_CacheMode {
          * - configure cache mode to REPLICATED
          * - create and return a cache with this configuration
          */
-        CacheConfiguration<Integer, String> configuration = new CacheConfiguration<>();
-        configuration.setName("my-replicated-cache");
-        configuration.setCacheMode(CacheMode.REPLICATED);
 
-        return ignite.getOrCreateCache(configuration);
+        return null;
     }
 
     public IgniteCache<Integer, String> createBackupCache(Ignite ignite) {
@@ -55,12 +46,8 @@ public class Step2_CacheMode {
          * - configure cache to have 1 backup exactly for each entry
          * - create and return a cache with this configuration
          */
-        CacheConfiguration<Integer, String> configuration = new CacheConfiguration<>();
-        configuration.setName("my-backup-cache");
-        configuration.setBackups(1);
-        configuration.setCacheMode(CacheMode.PARTITIONED);
 
-        return ignite.getOrCreateCache(configuration);
+        return null;
     }
 
     /*
@@ -74,7 +61,7 @@ public class Step2_CacheMode {
          * - return cache size in cluster (all keys in all nodes)
          * - use cache.size()
          */
-        return cache.size();
+        return null;
     }
 
     public Collection<Integer> getCacheSizePerNode(IgniteCache<Integer, String> cache) {
@@ -85,7 +72,7 @@ public class Step2_CacheMode {
          * - use cache.localSize(...)
          * - use ignite.compute().broadcast() to get cache size for each server node
          */
-        return ignite.compute().broadcast(() -> cache.localSize(CachePeekMode.ALL));
+        return null;
     }
 
     public Collection<Integer> getCacheSizePerNodeForPrimaryKeys(IgniteCache<Integer, String> cache) {
@@ -96,7 +83,7 @@ public class Step2_CacheMode {
          * - use cache.localSize(...)
          * - use ignite.compute().broadcast() to get cache size for each server node
          */
-        return ignite.compute().broadcast(() -> cache.localSize(CachePeekMode.PRIMARY));
+        return null;
     }
 
     public Collection<Integer> getCacheSizePerNodeForBackupKeys(IgniteCache<Integer, String> cache) {
@@ -107,7 +94,7 @@ public class Step2_CacheMode {
          * - use cache.localSize(...)
          * - use ignite.compute().broadcast() to get cache size for each server node
          */
-        return ignite.compute().broadcast(() -> cache.localSize(CachePeekMode.BACKUP));
+        return null;
     }
 
 }
